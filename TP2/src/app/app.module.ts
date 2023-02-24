c1285d9b9113c6d6cae3961adc151304d173e9d5
Félix Gagné <2070094@cegepmontpetit.ca>
2023-02-24 17:21:16 -0500

    Ajout du routing

--- TP2/src/app/app.module.ts
+++ TP2/src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ConcertComponent } from './concert/concert.component';
+import { AlbumComponent } from './album/album.component';
+import { SongComponent } from './song/song.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ConcertComponent,
+    AlbumComponent,
+    SongComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot([
+      {path: "", redirectTo: "/home", pathMatch: "full"},
+      {path: "home", component: HomeComponent},
+      {path: "album", component: AlbumComponent},
+      {path: "concert", component: ConcertComponent},
+      {path: "song", component: SongComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ TP2/src/app/concert/concert.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConcertComponent } from './concert.component';
+
+describe('ConcertComponent', () => {
+  let component: ConcertComponent;
+  let fixture: ComponentFixture<ConcertComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ConcertComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ConcertComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
